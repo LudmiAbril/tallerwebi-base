482d3ef1833db3333e4d0ce9658a0a01d3bac50f
Axel Daniel <axeelleguero@gmail.com>
2024-05-09 13:51:11 -0300

    Tests y pagina de chin

--- src/main/java/com/tallerwebi/presentacion/ControladorChin.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorChin.java
@@ -1,11 +1,33 @@
 package com.tallerwebi.presentacion;
 
+import com.tallerwebi.dominio.Carta;
+import com.tallerwebi.dominio.Jugador;
+import com.tallerwebi.dominio.ServicioChin;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class ControladorChin {
+    private ServicioChin servicioChin;
+
+    public ControladorChin(ServicioChin servicioChin){
+        this.servicioChin = servicioChin;
+    }
+
+    @RequestMapping(path = "/inicio-chin")
+    public ModelAndView inicioChin() {
+
+        ModelMap modelo = new ModelMap();
+        modelo.put("nuevoJugador", new Jugador());
+        return new ModelAndView("inicio-chin", modelo);
+    }
 
     public ModelAndView irAlChin() {
         String viewName = "chin";
@@ -14,5 +36,30 @@ public class ControladorChin {
         model.put("message", "Bienvenido al Chin");
 
         return new ModelAndView(viewName, model);
+    }
+    //public ModelAndView repartirCartas(){
+
+    //}
+
+    @RequestMapping(path = "/chin", method = RequestMethod.POST)
+    public ModelAndView comenzarChin(@ModelAttribute("nuevoJugador") Jugador nuevoJugador,
+                                          HttpSession session) {
+
+        // establesco lo valores de los masos
+        //List<Carta> cartasJugador = servicioChin.//entregarCartasPrincipales();
+        //List<Carta> cartasCasa = servicioChin.//entregarCartasPrincipales();
+
+        // guardo el nombre del jugador, los masos y los estados en la sesion
+        //session.setAttribute("jugadorActual", nuevoJugador.getNombre());
+        //session.setAttribute("cartasJugador", cartasJugador);
+        //session.setAttribute("cartasCasa", cartasCasa);
+        //session.setAttribute("estadoPartida", servicioBlackjack.estadoPartida(cartasJugador, cartasCasa, false));
+        //session.setAttribute("ganador",
+        //        servicioBlackjack.ganador(cartasJugador, cartasCasa, nuevoJugador.getNombre(), false));
+
+        // si estado partida "finalizado" llamo al servicioplataforma y guardo en la
+        // base
+        return new ModelAndView("chin");
+
     }
 }
--- src/test/java/com/tallerwebi/presentacion/ControladorChinTest.java
+++ src/test/java/com/tallerwebi/presentacion/ControladorChinTest.java
@@ -1,24 +1,36 @@
 package com.tallerwebi.presentacion;
 
+import com.tallerwebi.dominio.ServicioChin;
+import com.tallerwebi.infraestructura.ServicioChinImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.ui.Model;
 import org.springframework.web.servlet.ModelAndView;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.text.IsEqualIgnoringCase.equalToIgnoringCase;
+import static org.mockito.Mockito.when;
 
 public class ControladorChinTest {
     private ControladorChin controladorChin;
-
+    private ServicioChin servicioChin;
 
     @BeforeEach
     public void init(){
-        this.controladorChin = new ControladorChin();
+        this.servicioChin = new ServicioChinImpl();
+        this.controladorChin = new ControladorChin(servicioChin);
     }
     @Test
     public void queAlSolicitarLaVistaDeChinSeMuestreLaVistaDeChin(){
+        //when(this.servicioChin.get()).thenReturn();
         ModelAndView mav = controladorChin.irAlChin();
         String message = mav.getModel().get("message").toString();
         assertThat(mav.getViewName(), equalToIgnoringCase("chin"));
         assertThat(message, equalToIgnoringCase("Bienvenido al chin"));
+    }
+
+    @Test
+    public void queSeMuestrenLasCartasDelMazoRepartidas(){
+        //ModelAndView mav = this.controladorChin;
+
     }
 }
