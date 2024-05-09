d15d33149b203ef0e221e79124d6bb49a5003116
Celu Taumos <celena.ark@gmail.com>
2024-05-09 16:21:59 -0300

    test bingo

--- src/test/java/com/tallerwebi/presentacion/ControladorBingoTest.java
+++ src/test/java/com/tallerwebi/presentacion/ControladorBingoTest.java
@@ -1,5 +1,8 @@
 package com.tallerwebi.presentacion;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
@@ -22,7 +25,6 @@ import org.springframework.web.servlet.ModelAndView;
 import com.tallerwebi.dominio.CartonBingo;
 import com.tallerwebi.dominio.Jugador;
 import com.tallerwebi.dominio.ServicioBingo;
-import com.tallerwebi.infraestructura.ServicioBingoImpl;
 
 public class ControladorBingoTest {
 
@@ -38,15 +40,28 @@ public class ControladorBingoTest {
     }
 
     @Test
+    public void queAlSolicitarIrAlBingoSeEntregueLaVistaIrAlBingo() {
+        ModelAndView mav = this.controladorBingo.irAlBingo();
+        assertThat(mav.getViewName(), equalToIgnoringCase("irAlBingo"));
+    }
+
+    @Test
+    public void queAlSolicitarIrAlBingoSeGuardeElModeloCorrespondiente() {
+        ModelAndView mav = this.controladorBingo.irAlBingo();
+        Jugador MODELO_ACTUAL = ((Jugador) mav.getModel().get("nuevoJugador"));
+        assertThat(MODELO_ACTUAL, instanceOf(Jugador.class));
+    }
+
+    @Test
     public void queAlComenzarJuegoBingoSeRendericeLaVistaBingoGenerandoUnNumeroAleatorioEnLaSesion() {
+        //GIVEN
         CartonBingo cartonMock = mock(CartonBingo.class);
+        //WHEN
         when(servicioBingoMock.generarCarton()).thenReturn(cartonMock);
-
-        Jugador jugador = new Jugador(); // Crear un jugador de prueba
+        Jugador jugador = new Jugador();
         jugador.setNombre("NombreDePrueba");
-
-        ModelAndView modelAndView = controladorBingo.comenzarJuegoBingo(jugador, session); // Pasar el jugador al método
-
+        ModelAndView modelAndView = controladorBingo.comenzarJuegoBingo(jugador, session);
+        //THEN
         assertEquals("bingo", modelAndView.getViewName());
         assertNotNull(modelAndView.getModel().get("nombreJugador"));
         assertSame(cartonMock, session.getAttribute("carton"));
@@ -56,7 +71,6 @@ public class ControladorBingoTest {
     @Test
     public void queAlIrAVistaBingoSeRendericeLaVistaCorrectaYSeGuardeCorrectamenteUnJugador() {
         ModelAndView modelAndView = controladorBingo.irAlBingo();
-
         assertEquals("irAlBingo", modelAndView.getViewName());
         ModelMap modelMap = modelAndView.getModelMap();
         assertNotNull(modelMap);
@@ -65,27 +79,22 @@ public class ControladorBingoTest {
         assertTrue(jugadorObject instanceof Jugador);
         Jugador jugador = (Jugador) jugadorObject;
         assertNotNull(jugador);
-
     }
 
     @Test
     public void queAlObtenerDatosInicialesEncontremosUnCartonYElNumeroAleatorioEnLaSesion() {
-
         CartonBingo cartonMock = mock(CartonBingo.class);
         session.setAttribute("carton", cartonMock);
         Integer numeroCantadoAleatorio = 5;
         session.setAttribute("numeroAleatorioCantado", numeroCantadoAleatorio);
         Set<Integer> numerosEntregados = new HashSet<>();
         session.setAttribute("numerosEntregadosDeLaSesion", numerosEntregados);
-
         Map<String, Object> respuesta = controladorBingo.obtenerDatosIniciales(session);
-
         assertNotNull(respuesta);
         assertTrue(respuesta.containsKey("carton"));
         assertTrue(respuesta.containsKey("numeroAleatorioCantado"));
-
         CartonBingo cartonObtenido = (CartonBingo) respuesta.get("carton");
-        assertEquals(cartonMock, cartonObtenido); // Verifica si se devuelve el mismo cartón
+        assertEquals(cartonMock, cartonObtenido);
         assertEquals(numeroCantadoAleatorio, respuesta.get("numeroAleatorioCantado"));
     }
 }
