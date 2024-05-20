ade17fa0a1622a0e025576ff42a4c0c01bef4408
micazara <zara_micaela@outlook.com>
2024-05-20 18:31:41 -0300

    el numero cantado se agrega a los entregados antes de que se muestre el siguiente numero cantado

--- src/main/webapp/resources/core/js/bingo.js
+++ src/main/webapp/resources/core/js/bingo.js
@@ -48,12 +48,12 @@ function casilleroEsIgualANumeroEntregado(numeroCasillero, callback) {
 }
 
 function refrescarNumero() {
+    obtenerLosNumerosEntregados();
     $(".numeroCantadoContenedor").removeClass("w3-animate-top");
     setTimeout(function () {
         $.get("obtenerNuevoNumero", function (data) {
             $("#numeroCantado").text(data.nuevoNumero);
             $(".numeroCantadoContenedor").addClass("w3-animate-top");
-            obtenerLosNumerosEntregados();
         });
     }, 100); // Espera 100 milisegundos antes de solicitar el nuevo número
 }
