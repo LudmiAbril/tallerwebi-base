645337d9aad1c19337b5cc7cebfebb0901059393
Celu Taumos <celena.ark@gmail.com>
2024-05-22 18:03:39 -0300

    partida vista

--- src/main/java/com/tallerwebi/presentacion/ControladorAccesoJuegos.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorAccesoJuegos.java
@@ -1,9 +1,21 @@
 package com.tallerwebi.presentacion;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.tallerwebi.dominio.Juego;
+import com.tallerwebi.dominio.Partida;
+import com.tallerwebi.dominio.ServicioBlackjack;
+import com.tallerwebi.dominio.ServicioPlataforma;
+import com.tallerwebi.dominio.excepcion.PartidasDelJuegoNoEncontradasException;
+
+import java.util.List;
+
 
 @Controller
 public class ControladorAccesoJuegos {
@@ -13,4 +25,34 @@ public class ControladorAccesoJuegos {
 
         return new ModelAndView("acceso-juegos");
     }
+ //MISMO CONTROLADOR PARA EL TEMA DEL RANKING
+    private ServicioPlataforma servicioPlataforma;
+    @Autowired
+     public ControladorAccesoJuegos(ServicioPlataforma servicioPlataforma) {
+      
+        this.servicioPlataforma = servicioPlataforma;
+    }
+    @RequestMapping(path = "/verRanking" )
+    public ModelAndView verRanking(@RequestParam("tipoJuego") Juego tipoJuego ) {
+     
+        
+       ModelMap mav = new ModelMap();
+
+        Juego juego = tipoJuego;
+    
+    
+        List<Partida> partidas;
+        try {
+            partidas = servicioPlataforma.generarRanking(juego);
+            mav.addAttribute("partidas", partidas);
+        } catch (PartidasDelJuegoNoEncontradasException e) {
+            mav.addAttribute("mensajeError","todavía no hay instancias de partidas de este juego,¿por qué no las empezas?");
+           
+        }
+        
+        mav.addAttribute("nombreJuego",tipoJuego.toString());
+    
+        return new ModelAndView("ranking",mav);
+    }
+    
 }
