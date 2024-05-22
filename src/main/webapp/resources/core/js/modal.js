eb9ded821ed62b0b62fbfa2a9868da7fb3c5b39d
Celes Gomez <mmgomeez18@gmail.com>
2024-05-21 21:38:18 -0300

    modal salida

---
+++ src/main/webapp/resources/core/js/modal.js
@@ -0,0 +1,13 @@
+function openModal() {
+    document.getElementById('myModal').style.display = 'flex';
+}
+
+function closeModal() {
+    document.getElementById('myModal').style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById('myModal')) {
+        closeModal();
+    }
+}
