61fc38ef27c540a8bf9c328f1d4e586c5133a1ac
Axel Daniel <axeelleguero@gmail.com>
2024-06-05 13:43:45 -0300

    librerias de js

--- src/main/webapp/resources/core/js/bingo.js
+++ src/main/webapp/resources/core/js/bingo.js
@@ -209,3 +209,188 @@ function linea() {
 function abrirModalDeLimiteAlcanzado() {
     document.getElementById("modalLimite").style.display = "block";
 }
+
+//ACA VA LO DE WEBSOCKETS
+let stompClient = null;
+let game = null;
+let player = null;
+
+/**
+ * Sends a message to the server using the STOMP client.
+ * @param {Object} message - The message to be sent. Must contain at least a "type" field.
+ */
+const sendMessage = (message) => {
+    stompClient.send(`/app/${message.type}`, {}, JSON.stringify(message));
+}
+
+/**
+ * An object containing functions to handle each type of message received from the server.
+ */
+const messageHandlers = {
+    "bingo.join": (message) => {
+        updateGame(message);
+    },
+    "bingo.numberCalled": (message) => {
+        updateGame(message);
+    },
+    "bingo.winner": (message) => {
+        updateGame(message);
+        showWinner(message.winner);
+    },
+    "error": (message) => {
+        toastr.error(message.content);
+    }
+}
+
+/**
+ * Handles a message received from the server.
+ * @param {Object} message - The message received.
+ */
+const handleMessage = (message) => {
+    if (messageHandlers[message.type]) {
+        messageHandlers[message.type](message);
+    }
+}
+
+/**
+ * Connects the STOMP client to the server and subscribes to the "/topic/bingo" topic.
+ */
+const connect = () => {
+    const socket = new SockJS('/ws');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        stompClient.subscribe('/topic/bingo', function (message) {
+            handleMessage(JSON.parse(message.body));
+        });
+        loadGame();
+    });
+}
+
+/**
+ * Attempts to load a game by joining with the player's previously stored name, or prompts the player to enter their name if no name is stored.
+ */
+const loadGame = () => {
+    const playerName = localStorage.getItem("playerName");
+    if (playerName) {
+        sendMessage({
+            type: "bingo.join",
+            player: playerName
+        });
+    } else {
+        joinGame();
+    }
+}
+
+/**
+ * Starts the process of joining a game. Asks the player to enter their name and sends a message to the server requesting to join the game.
+ */
+const joinGame = () => {
+    const playerName = prompt("Enter your name:");
+    localStorage.setItem("playerName", playerName);
+    sendMessage({
+        type: "bingo.join",
+        player: playerName
+    });
+}
+
+/**
+ * Updates the game state with the information received from the server.
+ * @param {Object} message - The message received from the server.
+ */
+const updateGame = (message) => {
+    game = message;
+    updateBoard(message.board);
+    document.getElementById("player1").innerHTML = game.player1;
+    document.getElementById("player2").innerHTML = game.player2 || 'Waiting for player 2...';
+    document.getElementById("numeroCantado").innerHTML = game.currentNumber;
+    if (game.winner) {
+        showWinner(game.winner);
+    }
+}
+
+/**
+ * Displays a success message with the name of the winning player.
+ * @param {String} winner - The name of the winning player.
+ */
+const showWinner = (winner) => {
+    toastr.success(`The winner is ${winner}!`);
+    clearInterval(intervaloRefresco);
+}
+
+/**
+ * Calls a number and sends it to the server.
+ */
+const callNumber = () => {
+    $.get("obtenerNuevoNumero", function (data) {
+        if (!data.limiteAlcanzado) {
+            sendMessage({
+                type: "bingo.numberCalled",
+                number: data.nuevoNumero
+            });
+        } else {
+            abrirModalDeLimiteAlcanzado();
+        }
+    });
+}
+
+/**
+ * Marks a cell and sends it to the server.
+ * @param {Number} numeroCasillero - The number of the cell to be marked.
+ */
+const marcarCasillero = (numeroCasillero) => {
+    sendMessage({
+        type: "bingo.markCell",
+        number: numeroCasillero,
+        player: player
+    });
+}
+
+/**
+ * Calls bingo and sends it to the server.
+ */
+const bingo = () => {
+    sendMessage({
+        type: "bingo.call",
+        player: player
+    });
+}
+
+/**
+ * Get the initial game data and render the board.
+ */
+$(document).ready(function () {
+    connect();
+    $.get("obtenerDatosIniciales", function (data) {
+        $("#numeroCantado").text(data.numeroAleatorioCantado);
+        renderBoard(data.carton.numeros);
+    });
+    intervaloRefresco = setInterval(callNumber, 7000);
+});
+
+/**
+ * Renders the board based on the numbers provided.
+ * @param {Array} board - The board numbers.
+ */
+const renderBoard = (board) => {
+    let tablaHtml = "";
+    for (let i = 0; i < board.length; i++) {
+        tablaHtml += "<tr>";
+        for (let j = 0; j < board[i].length; j++) {
+            let numeroCasillero = board[i][j];
+            tablaHtml += `<td><button id='botonCasillero${numeroCasillero}' onclick='marcarCasillero(${numeroCasillero})'>${numeroCasillero}</button></td>`;
+        }
+        tablaHtml += "</tr>";
+    }
+    $(".carton").html(tablaHtml);
+}
+
+/**
+ * Shows a modal when the limit is reached.
+ */
+const abrirModalDeLimiteAlcanzado = () => {
+    document.getElementById("modalLimite").style.display = "block";
+}
+
+window.onload = function () {
+    connect();
+}
