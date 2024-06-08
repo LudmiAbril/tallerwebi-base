7a49ff7817970006c250f435c1c36cf92917472f
LudmiAbril <ludmila.pereyra543@gmail.com>
2024-06-08 15:34:14 -0300

    test and repo fix

--- src/test/java/com/tallerwebi/dominio/RepositorioUsuarioTest.java
+++ src/test/java/com/tallerwebi/dominio/RepositorioUsuarioTest.java
@@ -14,6 +14,12 @@ import com.tallerwebi.infraestructura.RepositorioUsuarioImpl;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+import static org.mockito.ArgumentMatchers.isNotNull;
+
+import org.apache.commons.codec.digest.DigestUtils;
 
 @Transactional
 @ExtendWith(SpringExtension.class)
@@ -42,4 +48,50 @@ public class RepositorioUsuarioTest {
         repositorioUsuario.guardar(user);
         assertThat(sessionFactory.getCurrentSession().contains(user), equalTo(true));
     }
+
+    @Test
+    public void queLaContraseñaDelUsuarioSeGuardeEncriptada() {
+        Usuario user = new Usuario();
+        user.setId(1L);
+        user.setEmail("user@gmail.com");
+        user.setNombre("usuario");
+        user.setConfig(new ConfiguracionesJuego());
+        user.setPassword("pass");
+        String passEncriptada = DigestUtils.sha512Hex(user.getPassword());
+
+        repositorioUsuario.guardar(user);
+        Usuario userGuardado = sessionFactory.getCurrentSession().get(Usuario.class, user.getId());
+        String passGuardada = userGuardado.getPassword();
+        assertThat(passGuardada, equalTo(passEncriptada));
+    }
+
+    @Test
+    public void queSePuedaBuscarUnUsuarioPorSuEmail() {
+        String email = "buscar@gmail.com";
+        Usuario user = crearObjetoUsuario("user", email, "123");
+        repositorioUsuario.guardar(user);
+
+        Usuario usuarioEncontrado = repositorioUsuario.buscar(email);
+        assertThat(usuarioEncontrado, is(notNullValue()));
+        assertThat(usuarioEncontrado, equalTo(user));
+    }
+
+    @Test
+    public void queSeNoSePuedaBuscarUnUsuarioPorSuEmailSiNoSeEncuentraEnLaBase() {
+        String email = "buscar@gmail.com";
+        Usuario user = crearObjetoUsuario("user", "otro-email@gmail.com", "123");
+        repositorioUsuario.guardar(user);
+
+        Usuario usuarioEncontrado = repositorioUsuario.buscar(email);
+        assertThat(usuarioEncontrado, is(nullValue()));
+    }
+
+    private Usuario crearObjetoUsuario(String nombre, String email, String pass) {
+        Usuario user = new Usuario();
+        user.setNombre(nombre);
+        user.setEmail(email);
+        user.setPassword(pass);
+        return user;
+    }
+
 }
