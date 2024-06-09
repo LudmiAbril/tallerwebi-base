e737999391c9f71535a3d4b7b2b16ad8cc91c21b
Axel Daniel <axeelleguero@gmail.com>
2024-06-08 22:52:45 -0300

    Arreglo de manejo de mensaje, ahora deberia mandar la info de los numeros

--- src/main/java/com/tallerwebi/presentacion/ControladorBingoWebsocket.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorBingoWebsocket.java
@@ -1,20 +1,42 @@
 package com.tallerwebi.presentacion;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+import javax.servlet.http.HttpSession;
+import java.util.HashSet;
+import java.util.Set;
+
 @Controller
 public class ControladorBingoWebsocket {
+    @Autowired
+    private HttpSession session;
 
     @MessageMapping("/bingo")
     @SendTo("/topic/updates")
     public BingoMessage handleBingoMessage(BingoMessage message) throws Exception {
-        // Lógica para manejar los mensajes de bingo
-        return message;
+        Set<Integer> numerosEntregados = (Set<Integer>) session.getAttribute("numerosEntregadosDeLaSesion");
+        if (numerosEntregados == null) {
+            numerosEntregados = new HashSet<>();
+        }
+        int nuevoNumero = Integer.parseInt(message.getContent());
+        numerosEntregados.add(nuevoNumero);
+        session.setAttribute("numerosEntregadosDeLaSesion", numerosEntregados);
+        BingoMessage response = new BingoMessage();
+        response.setContent(numerosEntregados.toString());
+        return response;
     }
 
     public static class BingoMessage {
         private String content;
-        // getters y setters
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
     }
 }
