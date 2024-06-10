7dd1c59dbf3df4f286e6bf238f9a08291c2d1bb6
micazara <zara_micaela@outlook.com>
2024-06-10 11:21:34 -0300

    test queNoSePuedaHacerLineaSiNoSeMarcoUnaLinea

--- src/test/java/com/tallerwebi/dominio/ServicioBingoTest.java
+++ src/test/java/com/tallerwebi/dominio/ServicioBingoTest.java
@@ -252,13 +252,53 @@ public class ServicioBingoTest {
 	}
 
 	@Test
-	public void queSePuedaObtenerLaCantidadDeNumerosRestantesParaCompletarLaTirada(){
+	public void queSePuedaObtenerLaCantidadDeNumerosRestantesParaCompletarLaTirada() {
 		Integer tirada = 50;
 		Integer numeroAleatorio = 10;
 		((ServicioBingoImpl) this.servicioBingo).getNumerosEntregados().add(numeroAleatorio);
-		Integer cantidadDeNumerosEntregados = ((ServicioBingoImpl) this.servicioBingo).obtenerCantidadDeNumerosEntregados();
-		Integer numerosRestantes = this.servicioBingo.obtenerCantidadDeNumerosRestantesParaCompletarLaTirada(this.servicioBingo.obtenerTirada(tirada), cantidadDeNumerosEntregados);
+		Integer cantidadDeNumerosEntregados = ((ServicioBingoImpl) this.servicioBingo)
+				.obtenerCantidadDeNumerosEntregados();
+		Integer numerosRestantes = this.servicioBingo.obtenerCantidadDeNumerosRestantesParaCompletarLaTirada(
+				this.servicioBingo.obtenerTirada(tirada), cantidadDeNumerosEntregados);
 		Integer numerosRestantesEsperados = 49;
 		assertThat(numerosRestantes, equalTo(numerosRestantesEsperados));
+	}
+
+	@Test
+	public void queNoSePuedaHacerLineaSiNoSeMarcoUnaLinea() {
+		// genero un carton
+		Integer[][] numeros = {
+				{ 1, 2, 3, 4, 5 },
+				{ 6, 7, 8, 9, 10 },
+				{ 11, 12, 13, 14, 15 },
+				{ 16, 17, 18, 19, 20 },
+				{ 21, 22, 23, 24, 25 }
+		};
+		CartonBingo carton = new CartonBingo(numeros);
+
+		((ServicioBingoImpl) this.servicioBingo).setDimension(5);
+		((ServicioBingoImpl) this.servicioBingo).setCartonNuevo(carton);
+
+		Integer numeroAleatorio1 = 1;
+		Integer numeroAleatorio6 = 6;
+		Integer numeroAleatorio11 = 11;
+		Integer numeroAleatorio16 = 16;
+		Integer numeroAleatorio21 = 21;
+
+		// te entrego numeros aleatorio que en el carton hagan linea
+		Set<Integer> numerosEntregados = new HashSet<Integer>();
+		((ServicioBingoImpl) this.servicioBingo).getNumerosEntregados().add(numeroAleatorio1);
+		((ServicioBingoImpl) this.servicioBingo).getNumerosEntregados().add(numeroAleatorio6);
+		((ServicioBingoImpl) this.servicioBingo).getNumerosEntregados().add(numeroAleatorio11);
+		((ServicioBingoImpl) this.servicioBingo).getNumerosEntregados().add(numeroAleatorio16);
+		((ServicioBingoImpl) this.servicioBingo).getNumerosEntregados().add(numeroAleatorio21);
+
+		Set<Integer> numerosMarcadosEnElCarton = new HashSet<Integer>();
+		numerosMarcadosEnElCarton.add(numeroAleatorio16);
+		numerosMarcadosEnElCarton.add(numeroAleatorio11);
+
+		// Verifico que no se haga línea
+		Boolean seHizoLinea = servicioBingo.linea(numerosMarcadosEnElCarton, carton);
+		assertThat(seHizoLinea, is(false));
 	}
 }
