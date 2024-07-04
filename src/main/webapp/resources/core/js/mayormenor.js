6c44faee872377753272b86bea904d80c1512eef
Celes Gomez <mmgomeez18@gmail.com>
2024-07-04 20:13:57 -0300

    modl

--- src/main/webapp/resources/core/js/mayormenor.js
+++ src/main/webapp/resources/core/js/mayormenor.js
@@ -86,3 +86,11 @@ function iniciarPartida() {
     });
     iniciarPartida();
 });
+
+function abrirModalAyuda() {
+    document.getElementById('modalAyuda').style.display = 'flex';
+}
+
+function cerrarModalAyuda() {
+    document.getElementById('modalAyuda').style.display = 'none';
+}
